a7396c65f9dfaae89cebf4c98d1108cb93dca155
Rebellewell <56415164+Rebellewell@users.noreply.github.com>
2019-11-07 20:38:48 -0700

    added colors array and improved buttons and formatting

---
+++ colors.js
@@ -0,0 +1,4 @@
+var colors = ['AliceBlue', 'AntiqueWhite', 'Aqua', 'Aquamarine', 'Azure', 'Beige', 'Bisque', 'CadetBlue', 'Coral', 'CornflowerBlue', 'Crimson',
+   'DarkCyan', 'DarkOrchid', 'DarkSalmon', 'Fuchsia', 'Gold', 'Grey', 'HotPink', 'Lavender', 'LightCoral', 'LightSeaGreen', 'MediumSpringGreen', 
+   'MintCream', 'MistyRose', 'PeachPuff', 'RosyBrown', 'Thistle', 'RebeccaPurple', 'Plum', 'Pink', 'Olive', 'Orchid', 'LightGreen'];
+
--- script.js
+++ script.js
@@ -1,22 +1,49 @@
 var buttonContainer = document.getElementById('button-container');
+var colorSelect = document.getElementById('select-color');
+var addBtn = document.getElementById('add-new');
 
-function addButton(color){
+initDropdownColors();
+
+function addButton(color) {
   var newButton = document.createElement('button');
-  newButton.innerText = color;
-  newButton.value = color;
-  newButton.style.backgroundColor = color;
-  //button.classList.add('button');
+  newButton.innerText = colorSelect.value;
+  newButton.value = colorSelect.value;
+  newButton.style.backgroundColor = colorSelect.value;
+  newButton.classList.add('button');
   buttonContainer.appendChild(newButton);
 }
 
-addButton('green');
-addButton('red');
-addButton('yellow');
+//Creating original buttons
+function ogButtons(color) {
+  var original = document.createElement ('button');
+  original.innerText = color;
+  original.value = color;
+  original.style.backgroundColor = color;
+  original.classList.add('button');
+  buttonContainer.appendChild(original);
+}
+
+ogButtons('Green');
+ogButtons('Red');
+ogButtons('Yellow');
 
 buttonContainer.addEventListener('click', colorChange);
 
 function colorChange(event) {
   var newColor = event.target.value;
   document.body.style.backgroundColor = newColor;
+}
+
+function initDropdownColors() {
+  for (var i = 0; i < colors.length; i++) {
+    addColor(colors[i]);
+  }
+}
+
+function addColor(color) {
+  var newColor = document.createElement('option');
+  newColor.innerText = color;
+  newColor.value = color;
+  colorSelect.appendChild(newColor);
 }
 
